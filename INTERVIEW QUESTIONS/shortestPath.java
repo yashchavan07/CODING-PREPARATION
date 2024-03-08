8640866d45be9e09eb5ca096f7dd1e2b3fc4b18c
yashchavan07 <112094448+yashchavan07@users.noreply.github.com>
2024-03-08 09:06:27 +0530

    Create shortestPath.java

---
+++ INTERVIEW QUESTIONS/shortestPath.java
@@ -0,0 +1,28 @@
+public class shortestPath {
+    public static float Path(String path){
+        int x=0, y=0;
+        for(int i=0; i<path.length(); i++){
+            char dir = path.charAt(i);
+            if(dir == 'N'){
+                y++;
+            }
+            else if(dir == 'S'){
+                y--;
+            }
+            else if(dir == 'W'){
+                x--;
+            }
+            else{
+                x++;
+            }
+        }
+        int Xsq = x*x;
+        int Ysq = y*y;
+        return (float)Math.sqrt(Xsq + Ysq);
+    }
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+        System.out.println(Path(path));
+    }
+    
+}
